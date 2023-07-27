252c86ae96fca47c5c176188395526527b0de7c8
HenriSousa <97175426+HenriSousa@users.noreply.github.com>
2023-07-27 16:41:25 -0300

    Update CodePark9.js

--- CodePark9.js
+++ CodePark9.js
@@ -2,3 +2,8 @@ let nome = "Fulano";
 let idade = 25;
 let altura = 1.88;
 let maior_de_idade = true;
+
+console.log("Nome: " + nome)
+console.log("Idade: " + idade)
+console.log("Altura: " + altura)
+console.log("É maior de idade? " + maior_de_idade)
