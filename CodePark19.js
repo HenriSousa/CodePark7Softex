28292ae317d0abb20aa1443a55aa3e813f47a3c0
HenriSousa <97175426+HenriSousa@users.noreply.github.com>
2023-08-04 12:11:53 -0300

    Update CodePark19.js

--- CodePark19.js
+++ CodePark19.js
@@ -3,9 +3,32 @@ let array = [3, 7, 9, 1, 0]
 function isEmpty(objeto) {
     return Object.keys(objeto).length === 0
 }
+// Formato de Fila (FIFO)
+while(!isEmpty(array)){
+    array.shift()
+}
+console.log("Array vazio (FIFO) ", array);
 
+
+// Formato de Lista
+function retirar(objeto) {
+    const tamanhoLista = array.indexOf(objeto);
+    if(tamanhoLista !== -1) {
+        array.splice(tamanhoLista, 1);
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log("Array vazio (Lista) ", 
+            retirar(3), retirar(7),
+            retirar(9), retirar(1),
+            retirar(0));
+
+// Formato de Pilha (LIFO)
 while(!isEmpty(array)){
     array.pop();
 }
 
-console.log("Array vazio", array)
+console.log("Array vazio (LIFO) ", array);
