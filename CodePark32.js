3e062fac32ef4578a8f4617dfb20c5b0bb913e06
HenriSousa <97175426+HenriSousa@users.noreply.github.com>
2023-09-01 11:44:57 -0300

    Add files via upload

---
+++ CodePark32.js
@@ -0,0 +1,15 @@
+let pessoa = [
+    nome = "Henrique",
+    idade = 28,
+    profissao = "Desenvolvedor",
+    cidade = "Recife"
+];
+
+for (let e in pessoa) {
+    console.log(e);
+}
+
+for (let e of pessoa) {
+    console.log(e);
+}
+
