8223aace317e1133822eb4996a3e8fc2929ec323
HenriSousa <97175426+HenriSousa@users.noreply.github.com>
2023-09-15 18:14:39 -0300

    Add files via upload

---
+++ CodePark40.js
@@ -0,0 +1,27 @@
+let pontuacao = [
+    {
+        nome: "João",
+        pontos: 50
+    },
+    {
+        nome: "Pedro",
+        pontos: 120
+    },
+    {
+        nome: "Luiza",
+        pontos: 70
+    },
+    {
+        nome: "Marcela",
+        pontos: 10
+    },
+    {
+        nome: "Yuri",
+        pontos: 34
+    }
+
+]
+
+pontuacao.forEach(element => {
+    console.log(element)
+});
