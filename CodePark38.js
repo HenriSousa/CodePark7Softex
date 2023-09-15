e0dcdb214256c00b3afd163684afea054fd331a0
HenriSousa <97175426+HenriSousa@users.noreply.github.com>
2023-09-15 17:28:01 -0300

    Add files via upload

---
+++ CodePark38.js
@@ -0,0 +1,8 @@
+let cadastro = [
+    ["José", 19, 1900, true],
+    ["David", 22, 2500, false],
+    ["Maria", 25, 3400, true],
+    ["Igor", 18, 2100, false]
+]
+
+console.log(cadastro[2][1]) // return 25
