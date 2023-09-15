883ed9ddac48a5ac07703a345f2c0a4e02fdf3cf
HenriSousa <97175426+HenriSousa@users.noreply.github.com>
2023-09-15 17:43:13 -0300

    Add files via upload

---
+++ CodePark39.js
@@ -0,0 +1,8 @@
+let capsLock = "MAIÚSCULO"
+let capsoff = "minúsculo"
+let separar = "     Fulano de Tal    "
+
+console.log(capsLock.toLowerCase()) // Coloca todas as letras em Minúsculo.
+console.log(capsoff.toUpperCase()) // Coloca todas as letras em Maiúsculo.
+console.log(separar.trim()) // Remove todos os espaços ao redor da String.
+console.log(separar.replaceAll("a", "1")) // Altera todas as letras a por 1.
